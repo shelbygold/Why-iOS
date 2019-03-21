c60db5f1430605cbb4efb8d62c08a59a442023cf
shelby gold <shelbygold@shelbys-MacBook-Air.local>
2019-03-20 20:31:07 -0600

    first commit

---
+++ Why iOS?/ModelControllers/WhyController.swift
@@ -0,0 +1,9 @@
+//
+//  WhyController.swift
+//  Why iOS?
+//
+//  Created by shelby gold on 3/20/19.
+//  Copyright © 2019 shelby gold. All rights reserved.
+//
+
+import Foundation
---
+++ Why iOS?/Views/WhyCustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WhyCustomTableViewCell.swift
+//  Why iOS?
+//
+//  Created by shelby gold on 3/20/19.
+//  Copyright © 2019 shelby gold. All rights reserved.
+//
+
+import UIKit
+
+class WhyCustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
